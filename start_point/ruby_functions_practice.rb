7b7203f0ae5f33209acea61cce5c8a6e1a87dd3d
Kanokwan Sritawan <KanokwanS@Kanokwans-MacBook-Pro.local>
2019-02-26 16:31:54 +0000

    adding ruby_functions_practice

---
+++ start_point/ruby_functions_practice.rb
@@ -0,0 +1,66 @@
+def return_10()
+  return 10
+end
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+def subtract(num1, num2)
+  return num1 - num2
+end
+
+def multiply(num1, num2)
+  return num1 * num2
+end
+
+def divide(num1, num2)
+  return num1 / num2
+end
+
+def length_of_string(test_string)
+  return test_string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(num1, num2)
+  return num1.to_i + num2.to_i
+end
+
+def number_to_full_month_name(num)
+  case num
+  when 1
+    "January"
+  when 2
+    "February"
+  when 3
+    "March"
+  when 4
+    "April"
+  when 5
+    "May"
+  when 6
+    "June"
+  when 7
+    "July"
+  when 8
+    "August"
+  when 9
+    "September"
+  when 10
+    "October"
+  when 11
+    "November"
+  when 12
+    "December"
+  else
+    "result"
+  end
+end
+
+def number_to_short_month_name(number)
+ return number_to_full_month_name(number).slice(0,3)
+end
